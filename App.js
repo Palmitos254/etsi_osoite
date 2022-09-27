a66b10c7c356b10b2e10fb1dbf25c6fb9cc3c9b4
Palmito254 <bgr921@myy.haaga-helia.fi>
2022-09-27 21:53:13 +0300

    valmis

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState("");
+
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0222,
+    longitudeDelta: 0.0121
+  });
+
+  const fetchOsoite = () => {
+    fetch("http://www.mapquestapi.com/geocoding/v1/address?key=I3Q8rIkGoFKXau6K3BTSqq9ShaTN9lZ0&location=" + keyword)
+    .then(response => response.json())
+    .then(data => setRegion({
+      latitude: data.results[0].locations[0].latLng.lat,
+      longitude: data.results[0].locations[0].latLng.lng,
+      latitudeDelta: 0.0222,
+      longitudeDelta: 0.0121
+    }))
+    .catch(err => Alert.alert("Error", err))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <MapView
+          style={{flex: 5}}
+          region={region} >
+          <Marker
+            coordinate={{latitude: region.latitude, longitude: region.longitude}} />
+        </MapView>
+        <TextInput
+          style={{flex: 1 ,fontSize: 18, width: 200}}
+          placeholder='Keyword'
+          onChangeText={text => setKeyword(text)}
+        />
+        <Button title='SHOW' onPress={fetchOsoite} />
     </View>
   );
 }
@@ -13,8 +46,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
